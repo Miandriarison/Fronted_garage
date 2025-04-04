0e21987618d7d9b31f49459771d78cfdf3e3b277
wardfarel <wardfarel@gmail.com>
2025-04-04 12:35:38 +0300

    insertion Voiture
    Insertion voiture

--- frontend/src/app/components/car-admin/car-admin.component.ts
+++ frontend/src/app/components/car-admin/car-admin.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { CarAdminService } from '../../services/car-admin.service';
 @Component({
   selector: 'app-car-admin',
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './car-admin.component.html',
   styleUrl: './car-admin.component.css'
 })
 export class CarAdminComponent {
-
+  cars: any[] = [];
+  marques: any[] = [];
+  newCar = { model: '', marque_id: '' };
+  constructor(private carAdminService: CarAdminService) {}
+  ngOnInit(): void {
+  this.loadCars();
+  this.loadMarques();
+  }
+  loadCars(): void {
+  this.carAdminService.getVoitures().subscribe(data => this.cars =data);
+  }
+  loadMarques(): void {
+  this.carAdminService.getMarques().subscribe(data => this.marques =data);
+  }
+  addCar(): void {
+    console.log(this.newCar)
+    if (this.newCar.model && this.newCar.marque_id) {
+    this.carAdminService.addVoiture(this.newCar).subscribe(() => {
+    this.loadCars(); // Recharge la listeaprès ajout
+    this.newCar = { model: '', marque_id: '' }; // Réinitialise leformulaire
+    });
+    }
+  }
 }
--- frontend/src/app/services/car-admin.service.ts
+++ frontend/src/app/services/car-admin.service.ts
@@ -1,9 +1,30 @@
+import { environment } from '../../environments/environment'; 
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarAdminService {
+  private apiUrl = `${environment.apiUrl}`; 
+  constructor(private http: HttpClient){ }
+  getVoitures(): Observable<any> {
+    return this.http.get(this.apiUrl+'/voiture/Recuperation');
+  }
+  getMarques(): Observable<any> {
+    return this.http.get(this.apiUrl+'/marque/Recuperation');
+  }
+  addVoiture(car: any): Observable<any> {
+    return this.http.post(this.apiUrl+"/voiture/Creation", car);
+  }
 
-  constructor() { }
+  // updateArticle(id: string, article: any): Observable<any> {
+  //   return this.http.put(`${this.apiUrl}/${id}`, article);
+  // }
+  // deleteArticle(id: string): Observable<any> {
+  //   return this.http.delete(`${this.apiUrl}/${id}`);
+  // }   
 }
+
+
