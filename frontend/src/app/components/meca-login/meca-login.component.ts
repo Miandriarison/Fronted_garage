2873a6319432b2e32324717b88e1d39de3c577d7
wardfarel <wardfarel@gmail.com>
2025-04-05 10:52:41 +0300

    Login mecanicien
    mandeha daholo ny login

--- frontend/src/app/components/meca-login/meca-login.component.ts
+++ frontend/src/app/components/meca-login/meca-login.component.ts
@@ -1,11 +1,43 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { MecaLoginService } from '../../services/meca-login.service';
+import { LocalStorageService } from '../../services/local-storage.service';
 
 @Component({
   selector: 'app-meca-login',
-  imports: [],
+  imports:  [CommonModule, FormsModule],
   templateUrl: './meca-login.component.html',
   styleUrl: './meca-login.component.css'
 })
 export class MecaLoginComponent {
-
+  login = { email: 'mec1@gmail.com', mdp: 'Mec' };
+  token='';
+  constructor(
+    private loginService: MecaLoginService, 
+    private localStorageService: LocalStorageService,
+    private router: Router
+    ) {}
+    ngOnInit(): void {
+    }
+    loginMecanicien(): void {
+      if (this.login.email && this.login.mdp) {
+        this.loginService.loginManager(this.login).subscribe((data) => {
+          // Affecter la valeur de token à this.token
+          if(data.message="bienvenue"){
+            this.token = data.token;
+            this.localStorageService.setItem('token', this.token);
+            this.router.navigate(['/1RDV-meca']);
+            console.log(this.token);  // Affiche le token dans la console
+          }
+          else console.log(data.message);
+        }, (error) => {
+          // Gérer les erreurs, si nécessaire
+          console.error('Erreur de connexion', error);
+        });
+      } else {
+        console.error('Veuillez saisir un email et un mot de passe');
+      }
+    }
 }
--- frontend/src/app/services/meca-login.service.ts
+++ frontend/src/app/services/meca-login.service.ts
@@ -1,9 +1,27 @@
+import { environment } from '../../environments/environment'; 
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MecaLoginService {
-
-  constructor() { }
+  private apiUrl = `${environment.apiUrl}/mecanicien/login`; 
+  constructor(private http: HttpClient){ }
+  loginManager(loginMecanicien: any): Observable<any> {
+    return this.http.post(this.apiUrl, loginMecanicien);
+  }
+  // getArticles(): Observable<any> {
+  //   return this.http.get(this.apiUrl);
+  // }
+  // addArticle(article: any): Observable<any> {
+  //   return this.http.post(this.apiUrl, article);
+  // }
+  // updateArticle(id: string, article: any): Observable<any> {
+  //   return this.http.put(`${this.apiUrl}/${id}`, article);
+  // }
+  // deleteArticle(id: string): Observable<any> {
+  //   return this.http.delete(`${this.apiUrl}/${id}`);
+  // }  
 }
