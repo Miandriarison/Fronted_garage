9946db538368f02b98aee0072e0c992ff491f7b8
wardfarel <wardfarel@gmail.com>
2025-04-05 11:28:50 +0300

    insertion manager
    ito ny insertion manager

--- frontend/src/app/components/inscription-admin/inscription-admin.component.ts
+++ frontend/src/app/components/inscription-admin/inscription-admin.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { InscriptionAdminService } from '../../services/inscription-admin.service';
+
 
 @Component({
   selector: 'app-inscription-admin',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './inscription-admin.component.html',
   styleUrl: './inscription-admin.component.css'
 })
 export class InscriptionAdminComponent {
-
+  newManager = { nom: '', prenom: '', email:'',datedenaissance:'', mdp:'', mdp1:'' };
+  constructor(private inscriptionAdminService: InscriptionAdminService,) {}
+  addManager(): void {
+    if (this.newManager.nom && this.newManager.prenom && this.newManager.email && this.newManager.mdp && this.newManager.mdp1 && this.newManager.datedenaissance) {
+      console.log(this.newManager);
+      this.inscriptionAdminService.addManager(this.newManager).subscribe(() => {
+      this.newManager = { nom: '', prenom: '', email:'',datedenaissance:'', mdp:'', mdp1:'' }; // Réinitialise leformulaire
+    });
+    }
+    }
 }
--- frontend/src/app/services/inscription-admin.service.ts
+++ frontend/src/app/services/inscription-admin.service.ts
@@ -1,9 +1,28 @@
+import { environment } from '../../environments/environment'; 
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InscriptionAdminService {
-
-  constructor() { }
+  private apiUrl = `${environment.apiUrl}`;
+  constructor(private http: HttpClient){ }
+  addManager(manager: any): Observable<any> {
+    return this.http.post(this.apiUrl+"/manager/Creation", manager);
+  }
+  // getVoitures(marque_id : String): Observable<any> {
+  //   return this.http.get(this.apiUrl+'/voiture/Recuperation/'+marque_id);
+  // }
+  // getMarques(): Observable<any> {
+  //   return this.http.get(this.apiUrl+'/marque/Recuperation');
+  // }
+  // updateArticle(id: string, article: any): Observable<any> {
+  //   return this.http.put(`${this.apiUrl}/${id}`, article);
+  // }
+  // deleteArticle(id: string): Observable<any> {
+  //   return this.http.delete(`${this.apiUrl}/${id}`);
 }
+
+
