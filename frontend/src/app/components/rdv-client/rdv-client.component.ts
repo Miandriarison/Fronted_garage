1a1ff1941b76f384d45f396349c199d8f4c3b6cd
wardfarel <wardfarel@gmail.com>
2025-04-07 09:33:51 +0300

    Reservation
    ito ilay reservation

--- frontend/src/app/components/rdv-client/rdv-client.component.ts
+++ frontend/src/app/components/rdv-client/rdv-client.component.ts
@@ -1,11 +1,63 @@
-import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { PieceAdminService } from '../../services/piece-admin.service';
+import { RdvClientService } from '../../services/rdv-client.service';
+import { LocalStorageService } from '../../services/local-storage.service';
 
 @Component({
   selector: 'app-rdv-client',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './rdv-client.component.html',
   styleUrl: './rdv-client.component.css'
 })
+// client_id:  { type: mongoose.Schema.Types.ObjectId, ref: 'client', required: true },
+//     voiture_id: { type: mongoose.Schema.Types.ObjectId, ref: 'voiture', required: true },
+//     adresse:{ type: String, required: true },
+//     description:{ type: String, required: true },
+//     tel: { type: String, required: true },
+//     etat:{type:Number ,required: true, validate:{
 export class RdvClientComponent {
+  newRdv = {marque_id:'', client_id: '', voiture_id: '', adresse:'', description:'', tel:'', etat:0 };
+  marques: any[] = [];
+  voitures: any[] = [];
+  constructor(
+    private pieceAdminService: PieceAdminService,
+    private rdvClientService: RdvClientService,
+    private localStorageService: LocalStorageService
+    ) {}
+  ngOnInit(): void {
+    this.loadMarques();
+    }
+    loadMarques(): void {
+      this.pieceAdminService.getMarques().subscribe((data) => {
+        this.marques =data;
+        console.log(this.marques);
+      });
+    }
+    onMarqueChange() {
+      // Filtrer les modèles selon la marque sélectionnée
+      if (this.newRdv.marque_id) {
+        this.pieceAdminService.getVoitures(this.newRdv.marque_id).subscribe((data) => {
+          this.voitures =data;
+        });
+      } else {
+        this.newRdv.marque_id ='' ;
+      }
+    }
+      addRdv(): void {
+        if (this.newRdv.tel && this.newRdv.voiture_id && this.newRdv.adresse && this.newRdv.description) {
+          this.rdvClientService.addRdv(this.newRdv,this.localStorageService.getItem("token")).subscribe(() => {
+          this.newRdv = {marque_id:'', client_id: '', voiture_id: '', adresse:'', description:'', tel:'', etat:0 }; // Réinitialise leformulaire
+        });
+        }
+        }
+  }
 
-}
+
+
+  
+
+
+
+
--- frontend/src/app/services/rdv-client.service.ts
+++ frontend/src/app/services/rdv-client.service.ts
@@ -1,9 +1,19 @@
+import { environment } from '../../environments/environment'; 
 import { Injectable } from '@angular/core';
+import { HttpClient,HttpHeaders  } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RdvClientService {
-
-  constructor() { }
+  private apiUrl = `${environment.apiUrl}`; 
+  constructor(private http: HttpClient) { }
+  addRdv(rdv: any,token: string): Observable<any> {
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`, // Ajouter le token dans les en-têtes
+      'Content-Type': 'application/json'
+    });
+    return this.http.post(this.apiUrl+"/reservation/Creation", rdv, {headers});
+  }
 }
